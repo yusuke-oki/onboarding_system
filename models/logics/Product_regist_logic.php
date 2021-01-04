ab0ded9acdb7babe3a591257ada17cf38c9ae0c5
Yusuke_Okinaka <yusuke-okinaka@with21.jp>
2021-01-04 15:03:08 +0900

    no message

--- models/logics/Product_regist_logic.php
+++ models/logics/Product_regist_logic.php
@@ -1,6 +1,27 @@
 <?php
 require_once "../models/dao/product_dao.php";
 require_once "../models/form/Product_category_form.php";
+/**
+ * 商品登録のロジック
+ * @package    onboarding_system
+ * @subpackage -
+ * @category   -
+ * @author     沖中  
+ * @link       -
+ */
 
+class Product_regist_logic
+{
+    /**
+     *商品の新規登録
+     *@param object $regist_item_object
+     */
+    public function item_regist($regist_item_object)
+    {
+
+    }
+
+
+}
 
 ?>
