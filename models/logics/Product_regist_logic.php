dd7b87272f8e2eea74f496487dcb4f72daceb2f0
Yusuke_Okinaka <yusuke-okinaka@with21.jp>
2021-01-04 17:54:55 +0900

    no message

--- models/logics/Product_regist_logic.php
+++ models/logics/Product_regist_logic.php
@@ -23,9 +23,49 @@ class Product_regist_logic
         try
         {
             $dbh = $product_dao->db_start();
-        } 
-    }
+        }
+        catch(PDOException $e)
+        {
+            $regist_item_object->set_message("DBの登録に失敗しました。");
+            throw $e;
+        }
+        $dbh->beginTransaction();
 
+        try
+        {
+            if($product_dao->get_upddatetime_for_update_by_itemcd($dbh, $regist_item_object->get_item_cd()))
+            {
+                // データが存在していたらロールバック
+                $regist_item_object->set_message("登録対象の商品データはすでに存在します。");
+                $dbh->rollBack();
+            }
+            else
+            {
+                if($product_dao->insert_item($dbh,$regist_item_object))
+                {
+                    $dbh->commit();
+                    $ret = true;
+                }
+                else
+                {
+                    throw new Exception("insertに失敗しました。");
+                }
+            }
+
+        }
+        catch(Exception $e)
+        {
+                $regist_item_object->set_message("DBの登録に失敗しました。");
+                $dbh->rollBack();
+                throw $e;
+        }
+        finally
+        {
+            $product_dao->db_close($dbh);
+        }
+        return $ret;
+    }
+    
 
 }
 
