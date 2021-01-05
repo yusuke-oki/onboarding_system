fec7aa11a0f8e5bb2660094baa3436a5f5d70ee1
Yusuke_Okinaka <yusuke-okinaka@with21.jp>
2021-01-05 13:35:55 +0900

    [fix]商品登録view修正

--- views/product_register.php
+++ views/product_register.php
@@ -13,31 +13,31 @@
         <div class="container" style="margin-top: 100px;">
             <div class="row">
                 <?php
-                   echo "<p class=\"mb-3\" </p>";
+                   echo "<p class=\"\" </p>";
                 ?>
             </div>
             <form action="../controllers/product_register_controller.php" method="post">
                 <div class="form-group">
-                    <div class="row">  
+                    <div class="row mt-5">  
                         <label for="item_cd" class="col-2">商品コード</label>
-                        <input class="ms-5 col-3" type="text" name="item_cd" id="item_cd" >
+                        <input class="ms-2 col-3" type="text" name="item_cd" id="item_cd" >
                     </div>
                     <div class="row mt-5">
                         <label for="item_name" class="col-2">商品名</label>
-                        <input class="ms-5 col-8" type="text" name="item_name" id="item_name" >
+                        <input class="ms-2 col-8" type="text" name="item_name" id="item_name" >
                     </div>
                     <div class="row mt-5">
                         <label for="itemdiv_selector" class="col-2">商品分類</label>
-                        <select name="itemdiv_selector" size=1 id="itemdiv_selector" class="ms-5 col-5"></select>
+                        <select name="itemdiv_selector" size=1 id="itemdiv_selector" class="ms-2 col-5"></select>
                     </div>
                     <div class="row mt-5">
                         <label for="unit_price" class="col-2">商品単価</label>
-                        <input class="ms-5 col-3" type="text" name="unit_price" id="unit_price">
+                        <input class="ms-2 col-3" type="text" name="unit_price" id="unit_price">
                         <div class="col-1">円</div>
                     </div>
                     <div class="d-flex row mt-5 justify-content-end  ">
                         <button class="col-2 btn btn-outline-success" name="btn_action" value="register" type="submit" onClick ="return regist_check()">登録
-                        <button class="col-2 ms-5 btn btn-outline-secondary" name="btn_action" value="pageback" type="submit">戻る
+                        <button class="col-2 ms-2 btn btn-outline-secondary" name="btn_action" value="pageback" type="submit">戻る
                     </div>
                 </div>
             </form>
